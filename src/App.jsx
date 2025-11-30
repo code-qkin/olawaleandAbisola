ebe925712c93c03b01f82c277de6daf297d56356
Apple <apple@Apples-MacBook-Pro.local>
2025-11-30 14:39:57 +0100

    invite function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WeddingPlanner from './Components/Weddingchecklist';
+import InviteGenerator from './Components/InviteGenerator';
+import InviteViewer from './Components/InviteViewer';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WeddingPlanner />} />
-        {/* You can add more routes here later, e.g.:
-        <Route path="/login" element={<Login />} />
-        <Route path="/guests" element={<GuestList />} />
-        */}
+        <Route path="/generate-invite" element={<InviteGenerator />} />
+        <Route path="/invite/:name" element={<InviteViewer />} />
       </Routes>
     </Router>
   );
